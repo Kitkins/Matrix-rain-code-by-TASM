107283ee65f705d2a3a27440ef23bfefeec848c6
Kitkins <31744036+Kitkins@users.noreply.github.com>
2018-02-17 23:56:21 +0700

    Update matrix.asm
    - remove backward character
    - delay it

--- matrix.asm
+++ matrix.asm
@@ -1,8 +1,8 @@
     .model tiny
     .data
-Char        db ?        ; Char and Color
 column      db 0        ; Set column to 0
 row         db 0        ; Set row to 0 
+delayCount dw ?
     .code
     org 0100h           ; for creating .com file
 main:
@@ -10,7 +10,6 @@ main:
     mov ah, 00h         ; Set video mode
     mov al, 03h         ; desired video mode
     int 10h
-
     
 rain:
     mov ah, 02h         ; Set cursor position       
@@ -25,20 +24,41 @@ rain:
     shr dl, 2           ; Try to slow down
     pop ax
     
-    mov  ax, dx
-    xor  dx, dx
-    mov  cx, 94
-    div  cx             ; here dx contains the remainder of the division - from 0 to 9
-    add  dl, '!'        ; to ascii from '0' to '9'
+    mov ax, dx
+    xor dx, dx
+    mov cx, 94
+    div cx             ; here dx contains the remainder of the division - from 0 to 9
+    add dl, '!'        ; to ascii from '0' to '9'
         
     mov ah, 09h
     mov al, dl
     mov bh, 00h
+    mov bl, 0Ah
+    mov cx, 1
+    int 10h
+    
+    mov delayCount, 50000   ; delay that using to see clearly 
+    mov cx, delayCount
+delay:
+    nop
+    dec delayCount
+    cmp delayCount, 0
+    jne delay
+        
+    mov ah, 09h         ; to remove backward character
+    mov al, ' '
+    mov bh, 00h
     mov bl, 04h
     mov cx, 01h
     int 10h
-
+    
+    inc row
+    
+    cmp row, 24
+    jg doExit
     jmp rain
+
+doExit:    
     
     ret
     end main
