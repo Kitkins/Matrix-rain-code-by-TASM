6ebc6b65b09e6a3820c1845dad2377a9ca7d4c1a
Kitkins <31744036+Kitkins@users.noreply.github.com>
2018-02-19 09:31:11 +0700

    Update matrix.asm
    -using call function and work

--- matrix.asm
+++ matrix.asm
@@ -1,8 +1,9 @@
     .model tiny
     .data
-Char        db ?        ; Char and Color
 column      db 0        ; Set column to 0
 row         db 0        ; Set row to 0 
+delayCount dw ?
+
     .code
     org 0100h           ; for creating .com file
 main:
@@ -11,30 +12,69 @@ main:
     mov al, 03h         ; desired video mode
     int 10h
 
-    
 rain:
+    call setCursor 
+    call basicRand
+    call printRandChar
+    call DelayF
+    call clearRandChar    
+    inc row
+    
+    cmp row, 24
+    jg doExit
+    jmp rain
+    
+setCursor:
     mov ah, 02h         ; Set cursor position       
     mov bh, 00h         ; bh for page number(0-7)
     mov dh, row         ; dh for row
     mov dl, column      ; dl for column
     int 10h
-    
+    ret    
+
+basicRand:
     push ax             ; Random Clr with Ticks of the Day
     mov ah, 00h
     int 1Ah
     shr dl, 2           ; Try to slow down
     pop ax
     
-    mov  ax, dx
-    xor  dx, dx
-    mov  cx, 94
-    div  cx             ; here dx contains the remainder of the division - from 0 to 9
-    add  dl, '!'        ; to ascii from '0' to '9'
-        
-    mov ah, 2
-    int 21h
+    mov ax, dx
+    xor dx, dx
+    mov cx, 94
+    div cx             ; here dx contains the remainder of the division - from 0 to 9
+    add dl, '!'        ; to ascii from '0' to '9'
+    ret    
 
-    jmp rain
+printRandChar:    
+    mov ah, 09h
+    mov al, dl
+    mov bh, 00h
+    mov bl, 0Ah
+    mov cx, 1
+    int 10h
+    ret
+
+DelayF:
+    mov delayCount, 50000   ; delay that using to see clearly 
+    mov cx, delayCount
+delay:
+    nop
+    dec delayCount
+    cmp delayCount, 0
+    jne delay
+    ret
+
+clearRandChar:  
+    mov ah, 09h         ; to remove backward character
+    mov al, ' '
+    mov bh, 00h
+    mov bl, 04h
+    mov cx, 01h
+    int 10h
+    ret
+
+doExit:    
     
     ret
     end main
